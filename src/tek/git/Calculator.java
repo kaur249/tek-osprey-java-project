1e3238699813dc041f5206c6282dfaa4d1f09c5d
Kulvinder Kaur <kaurk1909@gmail.com>
2024-04-02 23:02:36 -0400

    My Second changes

---
+++ src/tek/git/Calculator.java
@@ -0,0 +1,11 @@
+package tek.git;
+
+public class Calculator {
+    public static void main(String[] args) {
+        SecondClass obj = new SecondClass();
+
+        int result = obj.doAdding(50, 100);
+
+        System.out.println("Result is " + result);
+
+}
---
+++ src/tek/git/SecondClass.java
@@ -0,0 +1,6 @@
+package tek.git;
+
+public class SecondClass {
+    public int doAdding(int number, int secondNumber) {
+        return number + secondNumber;
+}
