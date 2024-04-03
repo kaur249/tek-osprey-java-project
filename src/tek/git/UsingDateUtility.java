cd42037a1bfa5702929ab6b3ececc5aae944365e
Kulvinder Kaur <kaurk1909@gmail.com>
2024-04-02 23:22:00 -0400

    Using Date Utility

--- src/tek/git/UsingDateUtility.java
+++ src/tek/git/UsingDateUtility.java
@@ -1,2 +1,12 @@
-package tek.git;public class UsingDateUtility {
+package tek.git;
+import tek.git.utility.DateUtility;
+
+import java.time.LocalDateTime;
+public class UsingDateUtility {
+    public static void main(String[] args) {
+        DateUtility utility = new DateUtility();
+        LocalDateTime now =utility.now();
+        System.out.println("Day of the Month is "
+                + now.getDayOfMonth());
+    }
 }
